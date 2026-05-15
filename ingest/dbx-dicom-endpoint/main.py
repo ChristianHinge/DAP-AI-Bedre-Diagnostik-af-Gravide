ac44a28825d5ed3f2c642f3a4e02fce6885c88e5
Christian Hinge <christian@hingepedersen.dk>
2026-05-15 15:18:25 +0200

    save zip instead of unzip

--- ingest/dbx-dicom-endpoint/main.py
+++ ingest/dbx-dicom-endpoint/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
 from databricks.sdk import WorkspaceClient
-import zipfile
-import io
 import os
 import secrets
 
@@ -16,12 +14,9 @@ PROCESSING_JOB_ID = int(os.environ.get("PROCESSING_JOB_ID",0))
 async def upload_study_zip(file: UploadFile = File(...)):
     study_id = secrets.token_hex(5)
     file_bytes = await file.read()
-    study_dir = f"{UC_VOLUME_PATH}"
-    
-    #os.makedirs(study_dir, exist_ok=True)
-
-    with zipfile.ZipFile(io.BytesIO(file_bytes)) as z:
-        z.extractall(study_dir)
+    zip_path = f"{UC_VOLUME_PATH}/{study_id}.zip"
+    with open(zip_path, "wb") as f:
+        f.write(file_bytes)
 
     run = _workspace.jobs.run_now(
         job_id=PROCESSING_JOB_ID,
